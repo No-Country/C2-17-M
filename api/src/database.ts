a1da617c30c25544065b2d5c980d884bd6c60c05
Gonnther <gonzalopintos680@gmail.com>
2022-01-21 12:49:49 -0300

    delete file .js

--- api/src/database.ts
+++ api/src/database.ts
@@ -1,8 +1,13 @@
 import mysql from 'promise-mysql';
 
-import keys from './keys'
+import dotenv from 'dotenv';
+dotenv.config()
 
-const pool =mysql.createPool(keys.database);
+const pool =mysql.createPool({
+		host     : process.env.HOST_SQL,
+        user     : process.env.USER_SQL,
+        password : process.env.PASS_SQL,
+        database : process.env.DB_SQL});
 
 pool.getConnection()
     .then(connection =>{
--- api/src/keys.ts
+++ api/src/keys.ts
@@ -1,9 +1,9 @@
 export default{
 
     database:{
-        host     : '156.67.72.51',
-        user     : 'u508931005_g17',
-        password : 'Cohorte2',
-        database : 'u508931005_g17'
+        host     : process.env.HOST_SQL,
+        user     : process.env.USER_SQL,
+        password : process.env.PASS_SQL,
+        database : process.env.DB_SQL
     }
 }
